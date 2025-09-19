c75072f925b757b18793564ab423799a83323182
pedro-h-martins <pedrohenrimartinss@gmail.com>
2025-09-19 09:55:05 -0300

    atualizar caminho para o mcp-config

--- Programacao/Codigo/server.py
+++ Programacao/Codigo/server.py
@@ -4,7 +4,7 @@ from mcp_use import MCPAgent, MCPClient
 from langchain_ollama import ChatOllama
 
 async def main():
-    config_path = os.path.join(os.path.dirname(__file__), "mcp-config.json")
+    config_path = os.path.join(os.path.dirname(__file__), "..\Json/mcp-config.json")
     client = MCPClient.from_config_file(config_path)
 
     llm = ChatOllama(model="qwen3:1.7b")
