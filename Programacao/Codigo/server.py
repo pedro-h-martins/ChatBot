62dfa3185fe3cdf70e65b670b9e2f051ad09e005
Gustavo Almeida Camargo <88459386+GustavoAC0802@users.noreply.github.com>
2025-09-19 09:40:15 -0300

    Estrutura inicial do ChatBot sem integração com API para devolver respostas de certo tema

---
+++ Programacao/Codigo/server.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+from mcp_use import MCPAgent, MCPClient
+from langchain_ollama import ChatOllama
+
+async def main():
+    config_path = os.path.join(os.path.dirname(__file__), "mcp-config.json")
+    client = MCPClient.from_config_file(config_path)
+
+    llm = ChatOllama(model="qwen3:1.7b")
+
+    agent = MCPAgent(
+        llm=llm,
+        client=client,
+        system_prompt="Assistant that can help answer user queries",
+        memory_enabled=True,
+        max_steps=30
+    )
+
+    result = await agent.run("Fetch the latest news headlines for me.")
+    print(result)
+
+if __name__ == "__main__":
+    asyncio.run(main())
